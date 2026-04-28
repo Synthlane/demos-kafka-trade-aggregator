e49247e5d816195947136f2a758e05c6ef455472
saurabh254 <sauravvishwakarma030@gmail.com>
2026-04-28 16:06:16 +0530

    feat(faust): Added faust processing configuration day 2.1

--- src/consumers/consumer.py
+++ src/consumers/consumer.py
@@ -1,24 +1,39 @@
+import faust
 
-from confluent_kafka import Consumer
-import socket 
-CONSUMER_ID = socket.gethostname() 
-c = Consumer({
-    'bootstrap.servers': 'localhost:19092',
-    'group.id':          'day1-group',
-    'auto.offset.reset': 'latest',
-    'enable.auto.commit': False 
-})
-c.subscribe(['trades'])
-count = 0 
-while True:
-    msg = c.poll(1.0)
-    if msg is None:
-        continue
-    print(f"[{CONSUMER_ID}] Partition: {msg.partition()} | Key: {msg.key().decode()} | Offset: {msg.offset()}")
+import json
 
-    count += 1
-    if count == 10:
-        raise Exception("Simulated crash!")
-    c.commit(msg)
- 
+app = faust.App(
+    'trade-processor',
+    broker='kafka://localhost:19092',
+    value_serializer='json',
+    topic_partitions=3
+)
 
+trades_topic = app.topic('trades', value_type=bytes)
+
+# This is the Table — distributed, fault-tolerant key-value state
+
+volume_table = app.Table('volume_per_symbol', default=float)
+
+@app.agent(trades_topic)
+async def process(stream):
+    async for msg in stream:
+        data = json.loads(msg) if not isinstance(msg, dict) else msg 
+        symbol = data['symbol']
+        qty    = data['qty']
+
+        volume_table[symbol] += qty
+
+        print(
+            f"{symbol} | "
+            f"this trade: {qty:.4f} | "
+            f"total volume: {volume_table[symbol]:.4f}"
+        )
+
+
+# * Where does the Table store its state?
+# - Faust write the data in the local first and then push to the kafka changelog.
+# * What is a changelog topic?
+# - changelog topic is a place where the faust store it's state. so that if any failure occurs it'll resume from the point of failure. 
+# * What is the difference between a Faust Table and a regular Python dict?
+# - Faust table first writes the data into the local then push it into the kafka. Where python has no point of recovery. even if we store the stats still it's not that durable.
